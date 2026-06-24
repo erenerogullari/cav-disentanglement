156a956ee8ba8336f0a0153392867cc8c5ccd419
erenerogullari <erenogullari@gmail.com>
2026-06-24 11:41:16 +0200

    use base config for ridge alignment sweep

--- scripts/run_concept_alignment.sh
+++ scripts/run_concept_alignment.sh
@@ -9,23 +9,23 @@ CKPT_PATH="checkpoints/checkpoint_vgg16_celeba_attacked.pth"
 
 CAV_MODE="max"                  # Options: full, max, avg
 OPTIMAL_INIT="true"             # true = CAV finetuning, false = training from scratch
-EXIT_CRITERION="auc"  # Options: None, orthogonality, auc
+EXIT_CRITERION="None"           # Options: None, orthogonality, auc
 NUM_EPOCHS="50"
 LR="0.0001"                     # Learning rate for CAV optimization
 SPLIT="test"                    # Options: train, val, test
 # MAX_SAMPLES="64"              # Optional debug cap
 
-ALPHAS=("0" "0.1" "1" "10")
+ALPHAS=("0" "0.1" "1" "10" "100")
 BETA="null"
 TARGET_CONCEPTS="[]"
 
 for ALPHA in "${ALPHAS[@]}"; do
   echo "Running Ridge CAV concept alignment with alpha=${ALPHA} and beta=${BETA}"
   python -m experiments.run_concept_alignment \
-    --config-name concept_alignment_ridge \
     hardware@train="${HARDWARE}" \
     model="${MODEL}" \
     model.ckpt_path="${CKPT_PATH}" \
+    cav_model@cav="ridge_cav" \
     cav.cav_mode="${CAV_MODE}" \
     cav.optimal_init="${OPTIMAL_INIT}" \
     cav.exit_criterion="${EXIT_CRITERION}" \
