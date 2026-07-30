e75ca1ed45c0a25a81efb90c7b274f200c3e3ccc
erogullari <eren.erogullari@hhi.fraunhofer.de>
2026-07-30 14:31:21 +0200

    update concept alignment plots

--- scripts/run_concept_alignment.sh
+++ scripts/run_concept_alignment.sh
@@ -20,45 +20,64 @@ BETAS=("0.1"  "1" "10" "100")
 TARGET_CONCEPTS="[timestamp, box, brightness]"
 
 for CAV_MODEL in "${CAV_MODELS[@]}"; do
-  for ALPHA in "${ALPHAS[@]}"; do
-    echo "Running ${CAV_MODEL} concept alignment with alpha=${ALPHA}"
-    python -m experiments.run_concept_alignment \
-      hardware@train="${HARDWARE}" \
-      model="${MODEL}" \
-      model.ckpt_path="${CKPT_PATH}" \
-      cav_model@cav="${CAV_MODEL}" \
-      cav.cav_mode="${CAV_MODE}" \
-      cav.optimal_init="${OPTIMAL_INIT}" \
-      cav.exit_criterion="${EXIT_CRITERION}" \
-      cav.layer="${LAYER}" \
-      train.num_epochs="${NUM_EPOCHS}" \
-      train.learning_rate="${LR}" \
-      alignment.split="${SPLIT}" \
-      cav.alpha="${ALPHA}" \
-      cav.beta="null" \
-      cav.target_concepts="[]" \
-      "$@"
-    echo "----------------------------------------------------------"
-  done
+  echo "Running ${CAV_MODEL} concept alignment with alpha=0.0"
+  python -m experiments.run_concept_alignment \
+    hardware@train="${HARDWARE}" \
+    model="${MODEL}" \
+    model.ckpt_path="${CKPT_PATH}" \
+    cav_model@cav="${CAV_MODEL}" \
+    cav.cav_mode="${CAV_MODE}" \
+    cav.optimal_init="${OPTIMAL_INIT}" \
+    cav.exit_criterion="${EXIT_CRITERION}" \
+    cav.layer="${LAYER}" \
+    train.num_epochs="10" \
+    train.learning_rate="${LR}" \
+    alignment.split="${SPLIT}" \
+    cav.alpha="0.0" \
+    cav.beta="null" \
+    cav.target_concepts="[]" \
+    "$@"
+  echo "----------------------------------------------------------"
 
-  for BETA in "${BETAS[@]}"; do
-    echo "Running ${CAV_MODEL} concept alignment with alpha=0 and beta=${BETA}"
-    python -m experiments.run_concept_alignment \
-      hardware@train="${HARDWARE}" \
-      model="${MODEL}" \
-      model.ckpt_path="${CKPT_PATH}" \
-      cav_model@cav="${CAV_MODEL}" \
-      cav.cav_mode="${CAV_MODE}" \
-      cav.optimal_init="${OPTIMAL_INIT}" \
-      cav.exit_criterion="${EXIT_CRITERION}" \
-      cav.layer="${LAYER}" \
-      train.num_epochs="${NUM_EPOCHS}" \
-      train.learning_rate="${LR}" \
-      alignment.split="${SPLIT}" \
-      cav.alpha="0" \
-      cav.beta="${BETA}" \
-      cav.target_concepts="${TARGET_CONCEPTS}" \
-      "$@"
-    echo "----------------------------------------------------------"
-  done
+  # for ALPHA in "${ALPHAS[@]}"; do
+  #   echo "Running ${CAV_MODEL} concept alignment with alpha=${ALPHA}"
+  #   python -m experiments.run_concept_alignment \
+  #     hardware@train="${HARDWARE}" \
+  #     model="${MODEL}" \
+  #     model.ckpt_path="${CKPT_PATH}" \
+  #     cav_model@cav="${CAV_MODEL}" \
+  #     cav.cav_mode="${CAV_MODE}" \
+  #     cav.optimal_init="${OPTIMAL_INIT}" \
+  #     cav.exit_criterion="${EXIT_CRITERION}" \
+  #     cav.layer="${LAYER}" \
+  #     train.num_epochs="${NUM_EPOCHS}" \
+  #     train.learning_rate="${LR}" \
+  #     alignment.split="${SPLIT}" \
+  #     cav.alpha="${ALPHA}" \
+  #     cav.beta="null" \
+  #     cav.target_concepts="[]" \
+  #     "$@"
+  #   echo "----------------------------------------------------------"
+  # done
+
+  # for BETA in "${BETAS[@]}"; do
+  #   echo "Running ${CAV_MODEL} concept alignment with alpha=0 and beta=${BETA}"
+  #   python -m experiments.run_concept_alignment \
+  #     hardware@train="${HARDWARE}" \
+  #     model="${MODEL}" \
+  #     model.ckpt_path="${CKPT_PATH}" \
+  #     cav_model@cav="${CAV_MODEL}" \
+  #     cav.cav_mode="${CAV_MODE}" \
+  #     cav.optimal_init="${OPTIMAL_INIT}" \
+  #     cav.exit_criterion="${EXIT_CRITERION}" \
+  #     cav.layer="${LAYER}" \
+  #     train.num_epochs="${NUM_EPOCHS}" \
+  #     train.learning_rate="${LR}" \
+  #     alignment.split="${SPLIT}" \
+  #     cav.alpha="0" \
+  #     cav.beta="${BETA}" \
+  #     cav.target_concepts="${TARGET_CONCEPTS}" \
+  #     "$@"
+  #   echo "----------------------------------------------------------"
+  # done
 done
